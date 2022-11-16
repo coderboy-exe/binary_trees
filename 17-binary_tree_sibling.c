3e8f3174eda933f1f69686a731b6516c18feaa0e
coderboy-exe <coderboy.exe@gmail.com>
2022-11-16 13:35:55 +0000

    make betty happy

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	
+
 	if (node == node->parent->left)
 		return (node->parent->right);
 	else
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	
+
 	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
 	else
